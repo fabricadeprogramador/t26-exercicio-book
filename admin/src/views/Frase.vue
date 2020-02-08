48a138cc064b7c6bacec76c9ec05d366d89cee2f
Virmerson Bento dos Santos <virmerson@hightechcursos.com.br>
2020-02-08 15:02:34 -0400

    Criando Tela de Frasex

--- admin/src/views/Frase.vue
+++ admin/src/views/Frase.vue
@@ -1,5 +1,59 @@
 <template>
-  <div>
-    <h1>Frase CRUD</h1>
-  </div>
+  <v-container>
+    <v-row>
+      <v-col cols="12" md="4">
+        <v-select
+          item-text="descricao"
+          item-value="_id"
+          :items="arrCategorias"
+          v-model="frase.categoria"
+          label="Categoria"
+        ></v-select>
+
+        <v-text-field label="Frase" v-model="frase.descricao"></v-text-field>
+        <v-btn @click="salvar()">Salvar</v-btn>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      frase: {},
+      arrCategorias: []
+    };
+  },
+  methods: {
+    salvar() {
+      axios
+        .post("http://localhost:3000/frase", this.frase)
+        .then(response => {
+          this.frase = {};
+          window.alert("Frase salva com sucesso");
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    },
+
+    buscarCategorias() {
+      axios
+        .get("http://localhost:3000/categoria")
+        .then(response => {
+          this.arrCategorias = response.data;
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    }
+  },
+
+  mounted() {
+    this.buscarCategorias();
+  }
+};
+</script>
